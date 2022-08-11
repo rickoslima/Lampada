ed2e1eaf178cc08c3b3b9c76a2ec6f7aee4f847f
Ricardo Lima <71652559+rickoslima@users.noreply.github.com>
2022-08-11 16:51:47 -0300

    Atualizando o botão Liga e desliga

--- lampada.js
+++ lampada.js
@@ -1,38 +1,43 @@
-const turnOn = document.getElementById('turnOn');
-const turnOff = document.getElementById('turnOff');
-const troca = document.getElementById('troca');
+const turnOnOff = document.getElementById('turnOnOff');
 const lamp = document.getElementById('lamp');
+const trocar = document.getElementById('troca');
 
-function isLampBroken (){
+function isLampBroken(){
     return lamp.src.indexOf ('quebrada') > -1
 }
-
-
 function lampOn (){
-    if(!isLampBroken()){
-    lamp.src = './img/ligada.jpg';
+    if (!isLampBroken () ){
+        lamp.src = './img/ligada.jpg';
     }
+    
 }
-
-function lampOff (){
-    if(!isLampBroken()){
+function lampOff(){
+    if (!isLampBroken () ){
     lamp.src = './img/desligada.jpg';
     }
 }
-
-function trocarlamp (){
-    
-    lamp.src = './img/desligada.jpg';
-    
-}
-
-function lampBroken (){
+function lampBroken(){
     lamp.src = './img/quebrada.jpg';
 }
+function NovaLamp(){
+    if (isLampBroken () ){
+        lamp.src = './img/desligada.jpg';
+    }   
+}
 
-turnOn.addEventListener('click', lampOn);
-turnOff.addEventListener('click', lampOff);
-troca.addEventListener('click', trocarlamp);
-lamp.addEventListener('mouseover', lampOn);
-lamp.addEventListener('mouseleave', lampOff);
-lamp.addEventListener('dblclick', lampBroken);
+function lampOnOff(){
+    if (turnOnOff.textContent == 'Ligar'){
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    }else{
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+
+
+turnOnOff.addEventListener ('click', lampOnOff);
+trocar.addEventListener ('click', NovaLamp);
+lamp.addEventListener ('mouseover', lampOn);
+lamp.addEventListener ('mouseleave', lampOff);
+lamp.addEventListener ('dblclick', lampBroken);
